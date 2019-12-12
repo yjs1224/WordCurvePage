32998c4b67abca536fc5c9d17cb16fb727be0e04
yangjs <1392918591@qq.com>
2019-12-12 17:57:37 +0800

    gh-pages

---
+++ precache-manifest.7c604aedfeb277c9ff1975199f19dcdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a6bc1f8998dd52a9cdc9919dc28707",
+    "url": "/WordCurvePage/index.html"
+  },
+  {
+    "revision": "6934d5b362243715d7f9",
+    "url": "/WordCurvePage/static/css/2.8a547ec4.chunk.css"
+  },
+  {
+    "revision": "6934d5b362243715d7f9",
+    "url": "/WordCurvePage/static/js/2.99fa0277.chunk.js"
+  },
+  {
+    "revision": "c729fe5973ae287f6531",
+    "url": "/WordCurvePage/static/js/main.44fda595.chunk.js"
+  },
+  {
+    "revision": "18a8f5c9e5c9bc2da725",
+    "url": "/WordCurvePage/static/js/runtime-main.de92ff6e.js"
+  },
+  {
+    "revision": "54d614c6a741d1d530664620b8351837",
+    "url": "/WordCurvePage/static/media/ngnlab.54d614c6.png"
+  }
+]);
